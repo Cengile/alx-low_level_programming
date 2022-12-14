4404c42009eca3ff567cd88b8dc008a45a14fe23
Cengile <inya.item@ncdc.gov.ng>
2022-12-14 00:24:44 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main Entry Point
  *
- * Return (0)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 
-	return 0;
+	return (0);
 }
