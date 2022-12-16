199316e368e249d16ed959174ed640c3c188d0b1
Cengile <inya.item@ncdc.gov.ng>
2022-12-16 04:24:15 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,7 +5,6 @@
  * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
- *
  */
 
 int main(void)
@@ -13,7 +12,7 @@ int main(void)
 
 	int i;
 
-	i= 98
+	i = 98
 	positive_or_negative(i);
 
 	return (0);
