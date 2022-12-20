9cee3c9ddb233a0ab9a7ed44af16f0dbdb1194b9
Cengile <inya.item@ncdc.gov.ng>
2022-12-20 05:31:33 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,56 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: The string to be modified
- *
- * Return: void
- */
+
+  * rev_string - Reverses a string
+
+  * @s: The string to be modified
+
+  *
+
+  * Return: void
+
+  */
+
 void rev_string(char *s)
+
 {
-	int i, j, k, temp;
-	i = 0
 
-	while (s[i] != '\0')
+	int i, c, k;
+
+	char *a, aux;
+
+
+	c = s;
+
+	while (s[c] != '\0')
+
 	{
-		i++;
+
+		c++;
+
 	}
-	k = 0;
-	j = i - 1
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
 
+	for (k = 1; k < c; k++)
+
+	{
+
+		a++;
+
+	}
+
+	for (i = 0; i < (c / 2); i++)
+
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		aux++;
+		a--;
 
 	}
 }
+
+
+
+
