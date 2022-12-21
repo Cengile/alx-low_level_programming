eda1e0507d1ac24943c19ee610bdd3ca2395c276
Cengile <inya.item@ncdc.gov.ng>
2022-12-21 07:42:31 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,17 +28,16 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 
 		{
-			digit = s[i] - '0';
+		digit = s[i] - '0';
 		if (d % 2)
-		digit = -digit;	
+		digit = -digit;
 		n = n * 10;
-		       	digit = 0;
+		digit = 0;
 		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;									
-		f = 0;		
+	if (s[i + 1] < '0' || s[i + 1] > '9')
+	break;
+		f = 0;
 		}
-		
 		i++;
 												}
 	if (f == 0)
