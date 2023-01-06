1fce1395849b76dec9649eeaa431287ca129dd7a
Cengile <inya.item@ncdc.gov.ng>
2023-01-06 08:23:28 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - prints prime factors of 612852475143
@@ -7,5 +8,17 @@
  */
 int main(void)
 {
+	int c;
+	long num = 612852475143;
+
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+
 	return (0);
 }
