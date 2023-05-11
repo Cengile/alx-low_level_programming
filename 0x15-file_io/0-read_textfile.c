04d3fafba707883fd5d9975cf14496454dca9e42
Cengile <inya.item@ncdc.gov.ng>
2023-05-11 02:12:07 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,36 @@
-#include <stddef.h>
 #include "main.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letter
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
-	int *buf;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	{
 	if (!filename)
 		return (0);
-	}
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
+
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
-	{
-		i = read(fd, buf, letters);
-	}
-	if (i < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[i] = '\0';
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
+
 	free(buf);
-	return (y);
+
+	return (nwr);
 }
