8c76073e8067ce77d67d58e7aca8fe7c6a09e121
Cengile <inya.item@ncdc.gov.ng>
2023-07-23 08:38:46 -0700

    libmy.c

--- 0x09-static_libraries/libmy.c
+++ 0x09-static_libraries/libmy.c
@@ -1,7 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-    _puts("\"At the end of the day, my goal is to create my first static library\"\n\t- Cengile");
-    return (0);
+	return (write(1, &c, 1));
 }
